25ba109ccb774482e714e2d9e6def698ea5b7581
adkif <akifungo@gmail.com>
2021-09-28 18:31:17 +0200

    update for continuous deployement

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+// //Install express server
+// const path = require("path");
+// const express = require("express");
+// const compression = require('compression')
 
-const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// const app = express();
+// app.use(express.static(__dirname + '/angular-build'));
+// app.get('/*', function(req,res){
+// res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+// });
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
